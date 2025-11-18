4a2ce7451c983d6a8113e9f089c18c35ec39be6b
Carol-zolet <165796798+Carol-zolet@users.noreply.github.com>
2025-11-18 13:53:05 -0300

    Add CORS configuration to main.ts
    Added CORS configuration to allow requests from specified origins.

--- caroline-desenvol-sistemas-backend/src/infra/main.ts
+++ caroline-desenvol-sistemas-backend/src/infra/main.ts
@@ -1,9 +1,23 @@
-﻿import express from 'express';
+import express from 'express';
+import cors from 'cors'; // <--- ADICIONADO
 import routes from './http/routes';
 import './workers/notification.worker';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// --- CONFIGURAÇÃO DE SEGURANÇA (CORS) ---
+app.use(cors({
+    origin: [
+        "http://localhost:5173",              // Para testar no seu PC
+        "http://localhost:3000",
+        "https://carolinenotificacoes.page",      // Seu domínio novo
+        "https://www.carolinenotificacoes.page",  // Seu domínio com WWW
+        "https://api.carolinenotificacoes.page"
+    ],
+    credentials: true
+}));
+// ----------------------------------------
 
 app.use(express.json());
 app.use('/api', routes);
