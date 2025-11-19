db2e3778f1b021d8400a03bcc87c52e25b8950ec
Carol Zolet <seu@email.com>
2025-11-19 14:40:23 -0300

    Reset de permissões

--- dist/worker.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Arquivo standalone para rodar o worker independentemente do servidor HTTP
-require("dotenv/config");
-const notification_worker_1 = require("./infra/workers/notification.worker");
-console.log("🤖 Iniciando Worker de Notificações...\n");
-const intervalMs = parseInt(process.env.NOTIFICATION_WORKER_INTERVAL_MS || "60000", 10);
-new notification_worker_1.NotificationWorker(intervalMs);
